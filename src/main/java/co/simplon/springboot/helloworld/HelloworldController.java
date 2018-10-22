8a5d645481c79b5c06a225ffe77a70f99c0f1ce0
vanessahuhn <vanessa.huhn@gmail.com>
2018-10-22 10:02:10 +0200

    Initial commit

---
+++ src/main/java/co/simplon/springboot/helloworld/HelloworldController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.simplon.springboot.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloworldController {
+	
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello()
+	{
+		return "Helloworld";
+	}
+}
---
+++ src/main/java/co/simplon/springboot/helloworld/SpringbootHelloworldApplication.java
@@ -0,0 +1,12 @@
+package co.simplon.springboot.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloworldApplication.class, args);
+	}
+}
